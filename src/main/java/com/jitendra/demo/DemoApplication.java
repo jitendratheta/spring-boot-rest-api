993ae68d77fe20c6ceb6a915a10ebeb4b88f8139
Jitendra Kushwaha <jitendra.theta@gmail.com>
2018-03-03 19:37:01 +0530

    removed auto configuration setting

--- src/main/java/com/jitendra/demo/DemoApplication.java
+++ src/main/java/com/jitendra/demo/DemoApplication.java
@@ -3,7 +3,6 @@ package com.jitendra.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @SpringBootApplication
 public class DemoApplication {
 
